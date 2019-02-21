6f5c1915f6486f20c1623ca75c5c399cff13dbd6
emil14 <emil.musician@gmail.com>
2019-02-21 16:02:11 +0300

    Add code and test logs

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-export default function asynctools() {
-  return {
-    map: (data, handler) => { },
-  };
+const worker = new Worker('worker.js');
+
+export function work(promises) {
+  return new Promise(resolve => {
+    worker.postMessage(promises);
+    worker.onmessage(e => resolve(e.data));
+  });
 }
+
+export function map(promises) {
+  const promises = data.map(async (...args) => handler(args));
+  return Promise.all(promises);
+}
+
+export default function asynctools() {
+  return { work, map };
+}
+
---
+++ test.js
@@ -0,0 +1,13 @@
+import { work, map } from './index.js';
+
+// this should return promise and resolve it in the main thread later
+console.log(
+  map([0, 1, 2], el => el * 2)
+)
+
+// this should return promise and resolve it in the worker later
+console.log(
+  work(
+    map([0, 1, 2], el => el * 2)
+  )
+)
